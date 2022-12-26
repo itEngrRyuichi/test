8c7b25a1aae4a64b5d1bd44d1e1e57e57f7fba94
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2022-12-26 23:30:23 +0900

    test

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>フジイカラーリプロ テストサイト</title>
+</head>
+<body>
+    <div class="global-navigation">
+        <div class="container">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">link 1</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">link 2</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">link 3</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">link 4</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="application-form container">
+        <form action="#">
+            <h1 class="main-title text-center my-5">ご注文・お見積り依頼書</h1>
+            <div class="orderAndQuotation-section d-flex justify-content-center">
+                <div class="form-check mx-3">
+                    <input type="checkbox" class="form-check-input" id="order">
+                    <label for="order" class="form-check-label">ご注文</label>
+                </div>
+                <div class="form-check mx-3">
+                    <input type="checkbox" class="form-check-input" id="quotation">
+                    <label for="quotation" class="form-check-label">お見積り</label>
+                </div>
+            </div>
+            <div class="orderer-section">
+                <label class="orderer-label mt-3 mb-1 px-5 d-flex">発注者</label>
+                <label class="print-title px-5">印刷物タイトル</label>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
